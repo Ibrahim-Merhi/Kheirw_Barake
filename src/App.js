ad486ea094339b0aaf87aa7a415cbc7b785f454c
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-12-13 11:46:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Nav/Navbar';
+import Hero from './components/Hero/Hero';
+import Homepage from './pages/homepage/Homepage';
+import Aboutus from './components/Aboutus/Aboutus';
+import Contactus from './components/Contactus/Contactus';
+import Error from './pages/Error/Error404';
+import Login from './pages/acc_creation/Login.js';
+import Signup from './pages/acc_creation/Signup';
+import Dashboard from './pages/dashboard/Dashboard';
+import Single from './components/Single/Single';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    // <div className="App">
+    //   <Navbar />
+    //   <Homepage /> 
+    //    <Dashboard /> 
+    //   <Single /> 
+    //   <Aboutus /> 
+    //   <Footer />
+    
+    
+    // </div>
+    <Router>
+    <div className="App">
+     <Navbar />
+    <Routes>
+          <Route exact path='/' element={< Homepage />}></Route>
+          <Route exact path='/about' element={< Aboutus />}></Route>
+          <Route exact path='/contact' element={< Contactus />}></Route>
+   </Routes>
+   </div>
+</Router>
+  );
+}
+
+export default App;
